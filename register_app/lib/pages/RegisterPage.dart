7a7d46e1395f3fb1b17fc81ec8e950b0240aa28d
Dennis Kievits <denkievits@gmail.com>
2019-04-25 23:00:11 +0200

    Basic register page

--- register_app/lib/pages/RegisterPage.dart
+++ register_app/lib/pages/RegisterPage.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
+import 'package:mobx/mobx.dart';
+import 'package:register_app/models/User.dart';
+import 'package:register_app/stores/UserStore.dart';
 
 class RegisterPage extends StatefulWidget {
   final String uuid;
@@ -26,10 +30,20 @@ class _RegisterPageState extends State<RegisterPage> {
   }
 
   void _fetchData() async {
+//    final response = await get('http://192.168.0.100/registration-details/$uuid');
+//    if (response.)
+
     await Future.delayed(Duration(milliseconds: 1500));
-    setState(() {
-      _isLoading = false;
+
+    final user = User(uuid: uuid, firstname: 'Dennis', lastname: 'Kievits');
+
+    final dispose = reaction((_) => userStore.user, (_) {
+      setState(() {
+        _isLoading = false;
+      });
     });
+    userStore.setUser(user);
+    dispose();
   }
 
   Widget _renderLoading() {
@@ -37,9 +51,7 @@ class _RegisterPageState extends State<RegisterPage> {
       crossAxisAlignment: CrossAxisAlignment.stretch,
       children: <Widget>[
         Expanded(
-          child: Center(
-            child: CircularProgressIndicator()
-          ),
+          child: Center(child: CircularProgressIndicator()),
         ),
         Container(
           margin: EdgeInsets.symmetric(vertical: 50),
@@ -54,7 +66,44 @@ class _RegisterPageState extends State<RegisterPage> {
   }
 
   Widget _renderContent() {
-    return Text("Content");
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: <Widget>[
+        Container(
+          color: Colors.grey,
+          padding: EdgeInsets.symmetric(vertical: 20),
+          child: Observer(
+            builder: (_) =>
+                Text(
+                  "${userStore.user.firstname} ${userStore.user.lastname}",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontSize: 21, color: Colors.white),
+                ),
+          ),
+        ),
+        Container(
+          margin: EdgeInsets.symmetric(horizontal: 30, vertical: 10),
+          child: Text(
+            "Voor het registratie proces hebben we een minimaal 10 foto's nodig van uw gezicht om u zo eenvoudig mogelijk toegang te kunnen bieden tot het evenement.",
+            textAlign: TextAlign.justify,
+            style: TextStyle(fontSize: 14),
+          ),
+        ),
+        Expanded(
+          child: Text("")
+        ),
+        Container(
+          margin: EdgeInsets.symmetric(horizontal: 50),
+          child: RaisedButton(
+            padding: EdgeInsets.symmetric(vertical: 15),
+            child: Text(
+              "Registreren",
+              style: TextStyle(fontSize: 16),
+            ),
+          ),
+        ),
+      ],
+    );
   }
 
   @override
